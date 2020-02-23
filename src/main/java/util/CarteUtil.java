2edf1725dd29cdbb1fa936d7c790d58deeacba20
gustavo.gois <G9o8i2s8!>
2020-02-23 21:26:14 +0000

    Refactoring architectural: CarteUtil

--- src/main/java/util/CarteUtil.java
+++ src/main/java/util/CarteUtil.java
@@ -5,7 +5,14 @@ import fr.com.gfi.deplacementcarte.model.Coordonner;
 
 public class CarteUtil {
 
-    public boolean nEstPasLimiteEst(Carte carte, Coordonner courant) {
-        return courant.getX() < (carte.getPoints()[courant.getY()].length - 1);
+    public boolean estLimiteEst(Carte carte, Coordonner courant) {
+        return courant.getX() >= (carte.getPoints()[courant.getY()].length - 1);
+    }
+
+    public boolean ilYABoisDroite(Carte carte, Coordonner courant) {
+        if (!estLimiteEst(carte, courant)) {
+            return carte.getPoints()[courant.getX() + 1][courant.getY()] == '#';
+        }
+        return false;
     }
 }
--- src/test/java/util/CarteUtilTest.java
+++ src/test/java/util/CarteUtilTest.java
@@ -10,20 +10,29 @@ import static org.junit.Assert.*;
 public class CarteUtilTest {
 
     CarteUtil util;
+    Carte carte;
 
     @Before
     public void setUp() throws Exception {
         this.util = new CarteUtil();
+        this.carte = new Carte(new Coordonner(2, 2));
     }
 
     @Test
     public void nEstPasLimiteEstTest() {
+        assertFalse(util.estLimiteEst(carte, new Coordonner(0, 0)));
+        assertFalse(util.estLimiteEst(carte, new Coordonner(0, 1)));
+        assertTrue(util.estLimiteEst(carte, new Coordonner(1, 0)));
+        assertTrue(util.estLimiteEst(carte, new Coordonner(1, 1)));
+    }
 
-        Carte carte = new Carte(new Coordonner(2, 2));
+    @Test
+    public void ilYABoisDroite() {
+        carte.setPoint(new Coordonner( 1, 0), '#');
 
-        assertTrue(util.nEstPasLimiteEst(carte, new Coordonner(0, 0)));
-        assertTrue(util.nEstPasLimiteEst(carte, new Coordonner(0, 1)));
-        assertFalse(util.nEstPasLimiteEst(carte, new Coordonner(1, 0)));
-        assertFalse(util.nEstPasLimiteEst(carte, new Coordonner(1, 1)));
+        assertTrue(util.ilYABoisDroite(carte, new Coordonner(0, 0)));
+        assertFalse(util.ilYABoisDroite(carte, new Coordonner(0, 1)));
+        assertFalse(util.ilYABoisDroite(carte, new Coordonner(1, 0)));
+        assertFalse(util.ilYABoisDroite(carte, new Coordonner(1, 1)));
     }
 }
