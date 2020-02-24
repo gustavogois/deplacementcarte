8daca4435beb68eabd5c3f1d03b883ed45e237ba
gustavo.gois <G9o8i2s8!>
2020-02-24 13:59:04 +0000

    Carte Service Refactoring

--- src/main/java/service/CarteService.java
+++ src/main/java/service/CarteService.java
@@ -1,5 +1,6 @@
 package service;
 
+import fr.com.gfi.deplacementcarte.model.Carte;
 import fr.com.gfi.deplacementcarte.model.Coordonner;
 import fr.com.gfi.deplacementcarte.model.Jeu;
 import util.CarteUtil;
@@ -10,17 +11,14 @@ public class CarteService {
     private CarteUtil Carteutil = new CarteUtil();
     private CoordonnerUtil coordUtil = new CoordonnerUtil();
 
-    public Jeu deplacement(Jeu jeu, char direction) {
+    public Coordonner deplacement(Carte carte, Coordonner courant, char direction) {
 
         // todo: immutability
         switch (direction) {
             case 'E' :
-                Coordonner nouveauCoord = Carteutil.estValable(jeu.getCarte(), jeu.getCourant(), direction) ?
-                        coordUtil.est(jeu.getCourant()) : jeu.getCourant();
-                jeu.setCourant(nouveauCoord);
-                return jeu;
+                return Carteutil.estValable(carte, courant, direction) ? coordUtil.est(courant) : courant;
             default:
-                return jeu;
+                return courant;
         }
     }
 
--- src/test/java/service/CarteServiceTest.java
+++ src/test/java/service/CarteServiceTest.java
@@ -11,17 +11,42 @@ import static org.junit.Assert.*;
 public class CarteServiceTest {
 
     CarteService service;
-    private Jeu jeu;
+    Carte carte;
+    Coordonner courant;
 
     @Before
     public void before() {
         this.service = new CarteService();
-        Carte carte = new Carte(new Coordonner(2, 2), ' ');
-        this.jeu = new Jeu(carte, new Coordonner(0, 0));
+        this.carte = new Carte(new Coordonner(2, 2), ' ');
+        this.carte.setPoint(new Coordonner(1, 0), '#');
+        this.courant = new Coordonner(0, 0);
     }
 
     @Test
     public void deplacementVersEst() {
+        this.courant = new Coordonner(0, 1);
 
+        Coordonner nouveauCoord = service.deplacement(this.carte, this.courant, 'E');
+
+        assertEquals(1, nouveauCoord.getX());
+        assertEquals(1, nouveauCoord.getY());
+    }
+
+    @Test
+    public void deplacementVersEstIlyaBois() {
+        Coordonner nouveauCoord = service.deplacement(this.carte, this.courant, 'E');
+
+        assertEquals(0, nouveauCoord.getX());
+        assertEquals(0, nouveauCoord.getY());
+    }
+
+    @Test
+    public void deplacementVersEstEstLimite() {
+        this.courant = new Coordonner(1, 1);
+
+        Coordonner nouveauCoord = service.deplacement(this.carte, this.courant, 'E');
+
+        assertEquals(1, nouveauCoord.getX());
+        assertEquals(1, nouveauCoord.getY());
     }
 }
